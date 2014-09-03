cbb5c6c82b0544d6d502581d07c5d88516e43a9e
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2014-09-03 13:35:19 +0200

    Structuration de la classe Entreprise.

--- AppliJEE/CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/data/model/Entreprise.java
+++ AppliJEE/CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/data/model/Entreprise.java
@@ -3,67 +3,44 @@ package eu.telecom_bretagne.cabinet_recrutement.data.model;
 import java.io.Serializable;
 import javax.persistence.*;
 
-
 /**
  * The persistent class for the entreprise database table.
  * 
  */
 @Entity
-@NamedQuery(name="Entreprise.findAll", query="SELECT e FROM Entreprise e")
-public class Entreprise implements Serializable {
-	@Override
-	public String toString() {
-		return "Entreprise [id=" + id + ", nom=" + nom + ", descriptif="
-				+ descriptif + ", adressePostale=" + adressePostale + "]";
-	}
+@NamedQuery(name = "Entreprise.findAll", query = "SELECT e FROM Entreprise e")
+public class Entreprise implements Serializable
+{
+  //-----------------------------------------------------------------------------
+  private static final long serialVersionUID = 1L;
+  @Id
+  @SequenceGenerator(name = "ENTREPRISE_ID_GENERATOR", sequenceName = "ENTREPRISE_ID_SEQ")
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ENTREPRISE_ID_GENERATOR")
+  private Integer id;
 
-	private static final long serialVersionUID = 1L;
+  @Column(name = "adresse_postale")
+  private String adressePostale;
 
-	@Id
-	@SequenceGenerator(name="ENTREPRISE_ID_GENERATOR", sequenceName="ENTREPRISE_ID_SEQ")
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="ENTREPRISE_ID_GENERATOR")
-	private Integer id;
+  private String descriptif;
 
-	@Column(name="adresse_postale")
-	private String adressePostale;
-
-	private String descriptif;
-
-	private String nom;
-
-	public Entreprise() {
-	}
-
-	public Integer getId() {
-		return this.id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getAdressePostale() {
-		return this.adressePostale;
-	}
-
-	public void setAdressePostale(String adressePostale) {
-		this.adressePostale = adressePostale;
-	}
-
-	public String getDescriptif() {
-		return this.descriptif;
-	}
-
-	public void setDescriptif(String descriptif) {
-		this.descriptif = descriptif;
-	}
-
-	public String getNom() {
-		return this.nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
+  private String nom;
+  //-----------------------------------------------------------------------------
+  public Entreprise() {}
+  //-----------------------------------------------------------------------------
+  public Integer getId()            { return this.id;             }
+  public String getNom()            { return this.nom;            }
+  public String getDescriptif()     { return this.descriptif;     }
+  public String getAdressePostale() { return this.adressePostale; }
+  //-----------------------------------------------------------------------------
+  public void setId(Integer id)                        { this.id             = id;             }
+  public void setNom(String nom)                       { this.nom            = nom;            }
+  public void setDescriptif(String descriptif)         { this.descriptif     = descriptif;     }
+  public void setAdressePostale(String adressePostale) { this.adressePostale = adressePostale; }
+  //-----------------------------------------------------------------------------
+  @Override
+  public String toString()
+  {
+    return "Entreprise [id=" + id + ", nom=" + nom + ", descriptif=" + descriptif + ", adressePostale=" + adressePostale + "]";
+  }
+  //-----------------------------------------------------------------------------
 }
